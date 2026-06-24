a1f26e485023ed6b5437d1ff158c1cc3d08bf886
kerberos79 <kerberos79@gmail.com>
2026-06-24 21:55:24 +0900

    v1.6.6

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1440,7 +1440,7 @@ Obx(() => rxUser.on(
                         color: AppTheme.textMuted, fontSize: 12.0),
                   ),
                   Text(
-                    'v1.6.5+21',
+                    'v1.6.6+22',
                     style: const TextStyle(
                         color: AppTheme.textMuted, fontSize: 12.0),
                   ),
